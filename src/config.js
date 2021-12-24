8587e934169c3c52da3261ae3b4578f85bd36840
JoshMTM <joshmitima08@yahoo.com>
2021-12-24 21:41:08 +0100

    change config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5005/api";
-// const API_URL = "saslife.herokuapp.com/api";
+// const API_URL = "http://localhost:5005/api";
+const API_URL = "saslife.herokuapp.com/api";
 
 export { API_URL };
